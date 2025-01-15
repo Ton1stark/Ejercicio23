6977d612663d1948740916a2ce8086828b19b08b
Dell <Dell@DESKTOP-H0TE2P9>
2025-01-14 21:57:20 -0500

    carga inicial

---
+++ src/ejerciciop23/EjercicioP23.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciop23;
+
+/**
+ *
+ * @author Dell
+ */
+public class EjercicioP23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      VentanaPrincipal ventana = new VentanaPrincipal();
+      ventana.setVisible(true);
+    }
+    
+}
---
+++ src/ejerciciop23/Soluciones.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciop23;
+
+/**
+ *
+ * @author Dell
+ */
+public class Soluciones {
+    double a, b, c;
+    
+public static double calcular_solucion1(double a, double b, double c){
+    
+
+    return ((-1) * b + Math.sqrt(Math.pow(b, 2)-4 * a * c))/2 * a ;
+        
+    }
+
+public static double calcular_solucion2(double a, double b, double c){
+    
+
+    return ((-1) * b -( Math.sqrt(Math.pow(b, 2) -4 * a * c)))/(2 * a);
+        
+    }
+
+public static String limpiar_campos(){
+    
+            return ""; 
+     }
+    
+}
